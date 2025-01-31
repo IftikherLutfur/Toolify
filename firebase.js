7c08d0cfa0f09d7ec579f14cc77aef9c26aaa472
Iftikher Lutfur <iftikherlutfur@gmail.com>
2025-01-31 12:03:30 +0600

    setup firebase

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaT8wA5eptmMs7OZNMGVgLD3QJfRS7m8g",
+  authDomain: "toolify-a1e47.firebaseapp.com",
+  projectId: "toolify-a1e47",
+  storageBucket: "toolify-a1e47.firebasestorage.app",
+  messagingSenderId: "395109908052",
+  appId: "1:395109908052:web:d0082d2a4e76d2016637c1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
